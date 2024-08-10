6cce71b02147767bf49b659d0eda7e8dcf663a18
Wyndermax <wynder.maxwell25@icloud.com>
2024-08-09 23:38:19 -0300

    ativ9

---
+++ atividade9.py
@@ -0,0 +1,7 @@
+frase = input("Digite uma frase:")
+
+letra = input("Digite uma letra que você deseja contar:")
+
+quant = frase.count(letra)
+
+print(f"Em sua frase a letra '{letra}' aparece {quant}.")
